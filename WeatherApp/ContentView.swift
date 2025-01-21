8d89e80aef0439ade4f7c656c76a19ac84558827
Gideonjon <gideonjones63@gmail.com>
2025-01-21 17:16:26 +0100

    weather-app-ui-added-now

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -1,4 +1,3 @@
-//
 //  ContentView.swift
 //  WeatherApp
 //
@@ -9,13 +8,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [Color.blue,Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("Cupertino, CA")
+                    .font(.system(size: 32,weight: .medium,design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack{
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180,height: 180)
+                    
+                }
+                
+                Spacer()
+            }
+          
         }
-        .padding()
+       
     }
 }
 
